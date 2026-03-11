7f6e7ac57e747286c9b4995f7f52ceca60d3bbda
anaera <mr.anaera@gmail.com>
2026-03-11 12:50:03 +0300

    Добавили header и поменяли выдачу младшим байтом вперед для добавдения в index OD

---
+++ _lm75/lm75.h
@@ -0,0 +1,21 @@
+
+#ifndef LM75_H
+#define	LM75_H
+
+#include <xc.h> // include processor files - each processor file is guarded.  
+#define TEMP_MAX 4
+
+typedef struct {
+    uint8_t lsbTemp;    //меняем выдачу на младшим байтом вперед !!!
+    uint8_t msbTemp;
+
+} msbLsb_t;
+
+typedef union {
+    msbLsb_t setTemp[TEMP_MAX]; // LM75 выдает temp Старшим байтом вперед
+    uint8_t packTemp[2*TEMP_MAX]; // меняем на младшим впкркд для int16
+    uint16_t temp16t[TEMP_MAX];
+} temp_t;
+
+#endif	/* LM75_H */
+
