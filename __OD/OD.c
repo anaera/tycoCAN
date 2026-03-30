83d3c806c35bff52933d8f5dbd24bb93a461b5eb
anaera <mr.anaera@gmail.com>
2026-03-30 20:02:51 +0300

    Добавили entry для параметров термостатов, PWM, func, EPP_RW

--- __OD/OD.c
+++ __OD/OD.c
@@ -10,6 +10,7 @@
 #include "../_pins/pins.h"
 #include "../__OD/OD.h"
 #include "../__CO/srvCTL.h"
+#include "../_lm75//lm75.h"
 
 #define T_SUB(X, Y) ((X) | ((Y) << 6))
 
@@ -21,20 +22,23 @@ uint8_t hard_version = 0x11;
 uint8_t soft_version = 0x11;
 uint8_t emcy;
 uint8_t state_node, state_send, state_prev;
-uint8_t ctlSYNC;
 uint8_t node_owner;
 uint8_t node_error;
-uint8_t tmp_Val, tmp_Max, tmp_Min, tmp_Pin;
+
+uint8_t by_event, by_request, by_timer;
 
 epp_sdo_t epp_sdo;
-
 uint8_t fun_val[4];
+
+temp_t tempVal; //пакет температуры для отсылки
+int8_t tmp_Max, tmp_Min;
+uint8_t num_termo;
 
 extern service_t srvHBT, srvSYNC;
 
 // размер словаря
-#define OD_MAX 12
-
+#define OD_MAX 13
+//uint8_t count_elements = sizeof(od) / sizeof(od[0]);
 uint8_t od_count = OD_MAX;
 
 // Субтаблицы
@@ -58,7 +62,9 @@ const subind_t sub_1017[] = {
 };
 const subind_t sub_1800[] = {
 	{ .num = 0, .type = T_SUB(INTEGER16, RW), .value = &srvSYNC.waitTime}, // период сихронизации PDO0
-	{ .num = 1, .type = T_SUB(INTEGER8, RW), .value = &ctlSYNC} //режимы сихронизации PDO0
+	{ .num = 1, .type = T_SUB(INTEGER8, RW), .value = &by_event}, //сихронизация по событию
+	{ .num = 2, .type = T_SUB(INTEGER8, RW), .value = &by_request}, //сихронизация по запросу
+	{ .num = 3, .type = T_SUB(INTEGER8, RW), .value = &by_timer} //сихронизация по периоду
 };
 const subind_t sub_1f80[] = {
 	{ .num = 0, .type = T_SUB(INTEGER8, RO), .value = &state_node},
@@ -68,10 +74,16 @@ const subind_t sub_2000[] = {
 	{ .num = 0, .type = T_SUB(INTEGER16, RW), .value = &dutyPWM} // заполнение PWM
 };
 const subind_t sub_2001[] = {
-	{ .num = 0, .type = T_SUB(INTEGER8, RO), .value = &tmp_Val},
+	{ .num = 0, .type = T_SUB(INTEGER16, RO), .value = &tempVal.temp16t[0]},
+	{ .num = 1, .type = T_SUB(INTEGER16, RW), .value = &tempVal.temp16t[1]},
+	{ .num = 2, .type = T_SUB(INTEGER16, RW), .value = &tempVal.temp16t[2]},
+	{ .num = 3, .type = T_SUB(INTEGER16, RW), .value = &tempVal.temp16t[3]}
+};
+const subind_t sub_2002[] = {
+	{ .num = 0, .type = T_SUB(INTEGER8, RW), .value = &num_termo},
 	{ .num = 1, .type = T_SUB(INTEGER8, RW), .value = &tmp_Max},
-	{ .num = 2, .type = T_SUB(INTEGER8, RW), .value = &tmp_Min},
-	{ .num = 3, .type = T_SUB(INTEGER8, RW), .value = &tmp_Pin}
+	{ .num = 2, .type = T_SUB(INTEGER8, RW), .value = &tmp_Min}
+
 };
 const subind_t sub_2201[] = {
 	{ .num = 0, .type = T_SUB(FUN_EXEC, WO), .value = &fun_val}
@@ -87,19 +99,20 @@ const od_entry_t od[] = {
 	{ .index = 0x1008, .sub_count = 1, .subs = &sub_1008[0]}, //hardware version
 	{ .index = 0x1009, .sub_count = 1, .subs = &sub_1009[0]}, //software version
 	{ .index = 0x1014, .sub_count = 1, .subs = &sub_1014[0]}, //EMCY
-	{ .index = 0x1017, .sub_count = 1, .subs = &sub_1017[0]}, //heartbeate
-	{ .index = 0x1800, .sub_count = 2, .subs = &sub_1800[0]}, //TPDO0
-	{ .index = 0x1f80, .sub_count = 2, .subs = &sub_1f80[0]}, // node_state
+	{ .index = 0x1017, .sub_count = 1, .subs = &sub_1017[0]}, //период heartbeate
+	{ .index = 0x1800, .sub_count = 4, .subs = &sub_1800[0]}, //параметры TPDO0
+	{ .index = 0x1f80, .sub_count = 2, .subs = &sub_1f80[0]}, // режим работу устр-ва (nodeState)
 	{ .index = 0x2000, .sub_count = 1, .subs = &sub_2000[0]}, // dutyPWM
-	{ .index = 0x2001, .sub_count = 4, .subs = &sub_2001[0]}, // temp00
-	{ .index = 0x2201, .sub_count = 1, .subs = &sub_2201[0]}, // fun_light
-	{ .index = 0x2202, .sub_count = 2, .subs = &sub_2202[0]} // epp_RW
+	{ .index = 0x2001, .sub_count = 4, .subs = &sub_2001[0]}, // температура с датчиков
+	{ .index = 0x2002, .sub_count = 3, .subs = &sub_2002[0]}, // установки термостата
+	{ .index = 0x2201, .sub_count = 1, .subs = &sub_2201[0]}, // команды управления светом
+	{ .index = 0x2202, .sub_count = 2, .subs = &sub_2202[0]} // чтение запись EPPROM
 };
 
-uint8_t count_elements = sizeof(od) / sizeof(od[0]);
 
 void updHBT(uint16_t newTime);
 void updSYNC(uint16_t newTime);
+
 /*
 void writePin(state_pin_t statePin);
 
@@ -120,17 +133,22 @@ void checkTemp0(uint8_t tempVal)
 	statePin.number = tempPin;
 	writePin(statePin);
 }
+ */
+void writePin(state_pin_t statePin);
 
-void checkTemp1(uint8_t tempVal)
+void termostat(uint8_t num)
 {
+	const uint8_t num_pin[4] = {7, 6, 3, 4};
 	state_pin_t statePin;
-	if (tempVal > tempMax || tempVal < tempMin) {
-		statePin.number = tempPin;
-		statePin.set.val = (tempVal > tempMax);
-		writePin(statePin);
+	if (num < num_termo) {
+		if (tempVal.temp16t[num] > tmp_Max || tempVal.temp16t[num] < tmp_Min) {
+			statePin.number = num_pin[num];
+			statePin.set.val = (tempVal.temp16t[num] > tmp_Max);
+			writePin(statePin);
+		}
 	}
 }
-*/
+
 void updPWM(uint16_t newDutyPWM)
 {
 	if (newDutyPWM > 500)return;
